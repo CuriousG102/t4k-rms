2c9274cb81d1a0f07fb8c79e31005de790c50b33
Miles <mileshutson@gmail.com>
2014-03-13 13:12:54 -0500

    Add URL for the Contest Winners page to dashboard/urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('dashboard.views',
     url(r'^logout/$', views.exit_gate, name = 'logout'),
     url(r'^log/ride/$', views.log_ride, name = 'log_ride'),
     url(r'^profile/change_password/$', views.change_password, name = 'change_password'),
+	url(r'^contest_winners/$', views.contest_winners, name = 'contest_winners'),
 )
